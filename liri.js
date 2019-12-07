debdb1d2a0e85c502b3f8a2878af6386800a8b03
Byanca De la fuente <byancadlf@gmail.com>
2019-12-06 19:53:03 -0600

    working bands in town api

--- liri.js
+++ liri.js
@@ -9,7 +9,8 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
-let value = process.argv[3];
+let value = process.argv.slice(3).join(" ");
+// let value = process.argv[3];
 
 switch(command){
     case 'movie-this': 
@@ -19,7 +20,7 @@ switch(command){
         song();
     break;
     case 'concert-this':
-        artist();
+        show();
     break;
 }
 
@@ -36,29 +37,44 @@ function movie(){
         console.log('Plot: ' + response.data.Plot)
         console.log('Actors: ' + response.data.Actors)
         // console.log('Rotten Tomatoes rating: ' + response.data.Ratings[i].source)
-        
+        // if(!value) {
+        //     value = "Mr. Nobody";
+        // } 
     })
+   
 }
 
-function artist(){
+function show(){
     let queryUrl = "http://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp"
     axios.get(queryUrl)
         .then(function(response){
-            console.log(response.data)
+            let jsonData = response.data[0];
+            console.log(jsonData)
+            let showData = [
+                "Venue Name: " + jsonData.venue.name,
+                "Venue Location: " + jsonData.venue.city
+            ].join('\n\n')
+            console.log(showData);
         })
+        
 }
 
-function song(){
+function song(response){
   spotify.search({ type: 'track', query: value }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
-      
+      console.log(response.data)
     }
+   
+      
+    
     let songs = data.tracks.items;
     for(let i = 0; i < songs.length; i++){
-        console.log(i);
         console.log('song name: ' + songs[i].name);
-        console.log('artist name: ' + songs[i].artist.name);
+        console.log('artist name: ' + songs[i].artist);
+        console.log('album name: ' + songs[i].album.name);
+        console.log('preview song: ' + songs[i].preview_url);
+        console.log('-------------------------------------------');
     }
   });
 }
